28639fb3f9018dba475653166d3b9351bfcf855d
ilhamfused <irharijanto20@gmail.com>
2025-11-06 09:24:33 +0700

    modif : style login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,24 @@
 @extends('layouts.auth_layout')
 @section('content')
     <!-- Nested Row within Card Body -->
-    <div class="p-5">
+    <div class="auth-card-body d-flex flex-column p-5 justify-content-center align-items-center">
         <div class="text-center">
-            <h1 class="h4 text-gray-900 mb-4">Masuk</h1>
+            @if (isset($setting) && !empty($setting->logo) && file_exists(public_path('uploads/logos/' . $setting->logo)))
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('uploads/logos/' . $setting->logo) }}" alt="{{ config('app.name', 'Lelang') }}"
+                        class="img-fluid mb-4 auth-logo">
+                </a>
+            @else
+                <a href="{{ route('home') }}" class="d-inline-block mb-3 text-decoration-none">
+                    <img src="{{ asset('assets/img/logo-lelang.png') }}" alt="{{ config('app.name', 'Lelang') }}"
+                        class="img-fluid mb-4 auth-logo">
+                </a>
+            @endif
         </div>
-        <form class="user" method="POST" action="{{ route('new.login') }}">
+        <form class="user mb-4 w-75" method="POST" action="{{ route('new.login') }}">
             @csrf
             <div class="form-group">
-                <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror"
+                <input type="email" class="form-control form-control-user text-black @error('email') is-invalid @enderror"
                     name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
                 @error('email')
                     <span class="invalid-feedback" role="alert">
@@ -26,24 +36,25 @@
                     </span>
                 @enderror
             </div>
-            <button type="submit" class="btn btn-danger btn-user btn-block">
+            <div class="text-right mb-2">
+                @if (Route::has('password.request'))
+                    <a class="font-weight-normal text-black" href="{{ route('password.request') }}">
+                        {{ __('Lupa Password?') }}
+                    </a>
+                @endif
+            </div>
+            <button type="submit" class="btn btn-black btn-user btn-block text-button-normal">
                 {{ __('Login') }}
             </button>
-            <hr>
             {{-- <a href="index.html" class="btn btn-google btn-user btn-block">
                 <i class="fab fa-google fa-fw"></i> Daftar dengan Google
             </a> --}}
         </form>
         {{-- <hr> --}}
+
         <div class="text-center">
-            @if (Route::has('password.request'))
-                <a class="small" href="{{ route('password.request') }}">
-                    {{ __('Lupa Password?') }}
-                </a>
-            @endif
+            <a class="font-weight-normal text-black" href="{{ route('register') }}">Belum memiliki akun? Daftar</a>
         </div>
-        <div class="text-center">
-            <a class="small" href="{{ route('register') }}">Belum memiliki akun? Daftar!</a>
-        </div>
+
     </div>
 @endsection
