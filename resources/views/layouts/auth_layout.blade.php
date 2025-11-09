88d2bc363911fbe6b5f1bb55ac72776370f3846b
Az-Zauqy <92534395+Az-Zauqy@users.noreply.github.com>
2025-11-09 14:20:11 +0700

    Update auth_layout.blade.php

--- resources/views/layouts/auth_layout.blade.php
+++ resources/views/layouts/auth_layout.blade.php
@@ -14,24 +14,14 @@
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
     <!-- Custom styles for this template-->
-
     {{-- <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet"> --}}
-
-    <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
-
     <link href="{{ asset('assets/css/style-auth.css') }}" rel="stylesheet">
 
 </head>
 
-<<<<<<< HEAD
 <body class="display-flex justify-content-center align-items-center min-vh-100">
 
     <div class="auth-container">
-=======
-<body class="d-flex justify-content-center align-items-center min-vh-100 py-5">
-
-    <div class="container p-0 auth-container justify-content-center align-items-center">
->>>>>>> Az-Zauqy
         <!-- close icon placed outside top-right corner -->
         <a href="{{ route('home') }}" class="auth-close" title="Close">
             <i class="far fa-times-circle"></i>
@@ -41,23 +31,6 @@
                 @yield('content')
             </div>
             <div class="auth-image"></div>
-
-        <!-- Outer Row -->
-        <!-- <div class="text-center">
-                    <a href="{{ route('home') }}">
-                        {{-- <img src="{{asset('assets/img/logo-lelang.png')}}"> --}}
-                        <img src="{{ asset('uploads/logos/' . $setting->logo) }}">
-                    </a>
-                </div> -->
-        <div class="row g-0 h-100">
-            <div class="col-md-6 auth-left h-100">
-                <div class="w-100">
-                    @yield('content')
-                </div>
-            </div>
-            <div class="col-md-6 d-none d-md-block h-100 p-0">
-                <div class="auth-image"></div>
-            </div>
         </div>
     </div>
     <!-- Bootstrap core JavaScript-->
@@ -70,6 +43,3 @@
 </body>
 
 </html>
-
-</html>
-
