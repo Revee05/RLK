9cbfe1f88874af3cad3113f7737fa1ae0cd2aede
Revee05 <125549900+Revee05@users.noreply.github.com>
2026-01-14 17:50:27 +0700

    Refactor CSS styles and improve layout structure for setting data page

--- resources/views/admin/setting/data.blade.php
+++ resources/views/admin/setting/data.blade.php
@@ -4,17 +4,32 @@
 @section('css')
 <style type="text/css">
 .preview-cover {
-    height:auto;
+    height: auto;
     width: 100%;
     overflow: hidden;
     position: relative;
-    border: 1px solid  #5a5c69;
+    border: 1px solid #5a5c69;
 }
-.preview-cover img{
-    height:100%;
+
+.preview-cover img {
+    height: 100%;
     width: 100%;
     object-fit: cover;
     object-position: center;
+}
+
+/* Separator between form and logo column */
+.logo-column {
+    border-left: 1px solid #e3e6f0;
+    padding-left: 20px;
+}
+
+@media (max-width: 767.98px) {
+    .logo-column {
+        border-left: none;
+        padding-left: 0;
+        margin-top: 15px;
+    }
 }
 </style>
 @endsection
@@ -23,20 +38,20 @@
 <div class="container-fluid">
     <!-- Page Heading -->
     <h1 class="h5 mb-4 text-gray-800">Setting
-    <small>Aplikasi</small>
-    {{-- <a href="" class="btn btn-primary btn-sm float-right"><i class="fa fa-plus-circle"></i> Create</a> --}}
+        <small>Aplikasi</small>
+        {{-- <a href="" class="btn btn-primary btn-sm float-right"><i class="fa fa-plus-circle"></i> Create</a> --}}
     </h1>
     {{ Form::open(array('route' => 'setting.update.data','files'=>true)) }}
-    <div class="row">
-        <div class="col-sm-6 order-2 order-sm-1">
-            
-            <div class="card shadow mb-4 rounded-0">
-                <div class="card-body">
+    <div class="card shadow mb-4 rounded-0">
+        <div class="card-body">
+            <div class="row">
+
+                <!-- kolom kiri -->
+                <div class="col-md-8">
                     <div class="form-group">
                         {{ Form::label('name', 'Title') }}
                         {{ Form::text('title', $setting->title, array('class' => 'form-control form-control-sm ','placeholder' => 'Nama Brand')) }}
                     </div>
-
                     <div class="form-group">
                         {{ Form::label('name', 'Tagline') }}
                         {{ Form::text('tagline', $setting->tagline, array('class' => 'form-control form-control-sm ','placeholder' => 'Nama Brand')) }}
@@ -57,32 +72,31 @@
                         {{ Form::label('name', 'Email') }}
                         {{ Form::text('email', $setting->email, array('class' => 'form-control form-control-sm ','placeholder' => 'Nama Brand')) }}
                     </div>
-                    
-                    
-                    
                     <input type="hidden" name="id" value="{{$setting->id}}">
-                    {{ Form::submit('Simpan', array('class' => 'btn btn-primary btn-sm rounded-0')) }}
-                    
                 </div>
-            </div>
-        </div>
-        <div class="col-sm-3 order-1 order-sm-2">
-            <div class="card rounded-0 border-0">
-                <div class="card-body p-0">
-                    <div class="preview-cover">
-                        <img class="border-1" @if(isset($setting) && $setting->logo) src="{{asset('uploads/logos/'.$setting->logo)}}" @else src="{{asset('assets/img/default.jpg')}}" @endif id="foto-logo">
+
+                <!-- kolom kanan -->
+                <div class="col-md-4 logo-column d-flex flex-column">
+                    <div class="preview-cover mb-3">
+                        <img class="border-1" @if(isset($setting) && $setting->logo)
+                        src="{{asset('uploads/logos/'.$setting->logo)}}" @else src="{{asset('assets/img/default.jpg')}}"
+                        @endif id="foto-logo">
                     </div>
-                    <div class="media-body m-auto">
-                        <input id="input-foto-logo" type="file" name="logo" class="d-none @error('logo') is-invalid @enderror" accept="image/*"/>
+                    <div class="media-body">
+                        <input id="input-foto-logo" type="file" name="logo"
+                            class="d-none @error('logo') is-invalid @enderror" accept="image/*" />
                         <label for="input-foto-logo" class="btn btn-sm btn-dark rounded-0 btn-block">
                             <i class="fa fa-folder-open"></i> Pilih Foto logo
                         </label>
-                        <p>Logo Size : 169x70</p>
+                        <p class="mt-2">Logo Size : 169x70</p>
                         @error('logo')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                         @enderror
+                    </div>
+                    <div class="mt-auto text-right">
+                        {{ Form::submit('Simpan', array('class' => 'btn btn-primary btn-sm rounded-0')) }}
                     </div>
                 </div>
             </div>
@@ -95,33 +109,33 @@
 <!-- /.container-fluid -->
 @endsection
 @section('js')
-    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-    <script type="text/javascript">
-         $(document).ready(function () {
-          'use strict';
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    'use strict';
 
-        //foto1
-        var previewFotologo = $('#foto-logo'),
-            fotologo = $('#input-foto-logo');
+    //foto1
+    var previewFotologo = $('#foto-logo'),
+        fotologo = $('#input-foto-logo');
 
-        //preview 1
-        if (fotologo) {
-            fotologo.on('change', function (e) {
-                var reader = new FileReader(),
+    //preview 1
+    if (fotologo) {
+        fotologo.on('change', function(e) {
+            var reader = new FileReader(),
                 files = e.target.files;
-                var fsize = files[0].size;
-                if(fsize > 2000000) {
-                    alert("Ukuran foto terlalu besar, maximal 2mb");
-                  } else {
-                    reader.onload = function () {
-                      if (previewFotologo) {
-                          previewFotologo.attr('src', reader.result);
-                      }
-                  };
-                    reader.readAsDataURL(files[0]);
-                }
-                  });
-            } 
-      });
-    </script>
+            var fsize = files[0].size;
+            if (fsize > 2000000) {
+                alert("Ukuran foto terlalu besar, maximal 2mb");
+            } else {
+                reader.onload = function() {
+                    if (previewFotologo) {
+                        previewFotologo.attr('src', reader.result);
+                    }
+                };
+                reader.readAsDataURL(files[0]);
+            }
+        });
+    }
+});
+</script>
 @endsection
