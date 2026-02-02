e0deb7f9cc804ecdb5125af73d7535cb66489739
Az-Zauqy <92534395+Az-Zauqy@users.noreply.github.com>
2026-02-03 01:39:57 +0700

    perbaikan beranda

--- routes/admin.php
+++ routes/admin.php
@@ -12,6 +12,7 @@
 */
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Auth::routes(['verify' => true]);
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
